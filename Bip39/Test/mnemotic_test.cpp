03dd96c62d9a7966ba81b1089d29f9074f574f16
Eztero <37254547+Eztero@users.noreply.github.com>
2022-06-25 22:29:33 -0400

    the example for the entropy2mnemotic() function is added

--- Bip39/Test/mnemotic_test.cpp
+++ Bip39/Test/mnemotic_test.cpp
@@ -4,20 +4,44 @@
 #include <iomanip>
 int main(int argc, char *argv[]){
     
-    std::string mnemotic="blanket pistol rough habit hockey virus protect page bread buffalo duty canoe six clarify scare arrive december device page silly attend defense elder icon";
+    if (sodium_init() < 0) {
+        std::cout<<"panic! the library couldn't be initialized; it is not safe to use"<<std::endl;
+        return 0;
+    }
     
-    std::uint8_t entropy_length_bytes=0;
-    std::uint8_t *entropy=mnemotic2entropy(mnemotic.c_str(),Diccionary::English, &entropy_length_bytes);
+    std::string mnemotic = "blanket pistol rough habit hockey virus protect page bread buffalo duty canoe six clarify scare arrive december device page silly attend defense elder icon";
+    std::uint8_t entropy_length_bytes = 0;
+    std::uint8_t mnemotic_length = 0;
+    
+    ///----- Mnemotic to entropy -----
+    std::uint8_t *entropy = mnemotic2entropy(mnemotic.c_str(),Diccionary::English, &entropy_length_bytes);
     if(entropy!=nullptr){
         std::cout<<"\nentropy: ";
-        for(std::uint8_t a=0;a<entropy_length_bytes;a++){
+        for(std::uint8_t a = 0; a < entropy_length_bytes; a++){
             std::cout<<std::hex <<std::setfill('0')<<std::setw(2) <<static_cast<int>(entropy[a]);
         }
         std::cout<<std::endl;
-        free(entropy);
+        
     }
     else{
         std::cout<<"invalid mnemotic"<<std::endl;
     }
+    
+    ///----- Entropy to mnemotic -----
+    char *mnemotics = entropy2mnemotic(entropy, &entropy_length_bytes, Diccionary::English, &mnemotic_length);
+    if(mnemotics!=nullptr){
+       std::cout<<"Mnemotic: "<<mnemotics<<std::endl;
+    }
+    else{
+        std::cout<<"invalid Entropy"<<std::endl;
+    }
+    
+    ///----- Delete data before freeing memory -----
+    sodium_memzero(entropy, entropy_length_bytes);
+    sodium_memzero(mnemotics, mnemotic_length);
+    
+    free(entropy);
+    free(mnemotics);
+    
     return 0;
 }
