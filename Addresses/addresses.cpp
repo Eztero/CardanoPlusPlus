7cc0288959820a5a300a367d7c43207d4b93666f
Eztero <37254547+Eztero@users.noreply.github.com>
2022-08-11 17:19:29 -0400

    license and minor changes are added

--- Addresses/addresses.cpp
+++ Addresses/addresses.cpp
@@ -2,65 +2,88 @@
 
 static bool set_address(Address const *const address_type, std::uint8_t const *const input_key, InputKey const *const input_key_type, Wallet const * const wallet_type,
                         std::uint32_t const *const account_path, std::uint32_t const *const address_index_path, std::uint8_t *const header,
-                        std::uint8_t *const playload_left, std::uint8_t *const playload_right,
+                        std::uint8_t *const payload_left, std::uint8_t *const payload_right,
                         std::uint8_t *const buff_xvk){
 
     ///Set Address
-    switch(*address_type){             ///bech32(hrp,(header || playload_left || playload_right))
+    switch(*address_type){             ///bech32(hrp, (header || payload_left || payload_right) )
+
     case Address::Base:{              /// Role = External Address
 
-        raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
-                      *account_path, Role::Extern, *address_index_path, buff_xvk);
+        if(!raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
+                          *account_path, Role::Extern, *address_index_path, buff_xvk)){
+            return false;
+        }
 
-        crypto_generichash_blake2b(playload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
+        crypto_generichash_blake2b(payload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
-        raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
-                      *account_path, Role::Staking, STAKE_INDEX, buff_xvk);
+        if(!raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
+                          *account_path, Role::Staking, STAKE_INDEX, buff_xvk)){
+            return false;
+        }
 
-        crypto_generichash_blake2b(playload_right, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
+        crypto_generichash_blake2b(payload_right, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
     };break;
+
     case Address::Base_Change:{       /// Role = Internal Address
 
-        raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
-                      *account_path, Role::Intern, *address_index_path, buff_xvk);
+        if(!raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
+                          *account_path, Role::Intern, *address_index_path, buff_xvk)){
+            return false;
+        }
 
-        crypto_generichash_blake2b(playload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
+        crypto_generichash_blake2b(payload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
-        raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
-                      *account_path, Role::Staking, STAKE_INDEX, buff_xvk);
+        if(!raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
+                          *account_path, Role::Staking, STAKE_INDEX, buff_xvk)){
+            return false;
+        }
 
-        crypto_generichash_blake2b(playload_right, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
+        crypto_generichash_blake2b(payload_right, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
 
     };break;
+
     case Address::Enterprise:{        /// Role = External Address
+
         *header |= 0x60; //b0110_0000
 
-        raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
-                      *account_path, Role::Extern, *address_index_path, buff_xvk);
+        if(!raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
+                          *account_path, Role::Extern, *address_index_path, buff_xvk)){
+            return false;
+        }
 
-        crypto_generichash_blake2b(playload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
+        crypto_generichash_blake2b(payload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
     };break;
+
     case Address::Enterprise_Change:{ /// Role = Internal Address
+
         *header |= 0x60; //b0110_0000
 
-        raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
-                      *account_path, Role::Intern, *address_index_path, buff_xvk);
+        if(!raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
+                          *account_path, Role::Intern, *address_index_path, buff_xvk)){
+            return false;
+        }
 
-        crypto_generichash_blake2b(playload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
+        crypto_generichash_blake2b(payload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
     };break;
+
     case Address::Stake:{             /// Role = Staking Address = External Address
+
         *header |= 0xE0; //b1110_0000
 
-        raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
-                      *account_path, Role::Staking, *address_index_path, buff_xvk);
+        if(!raw_derivekey(input_key, *input_key_type, *wallet_type, OutputKey::Public,
+                          *account_path, Role::Staking, *address_index_path, buff_xvk)){
+            return false;
+        }
 
-        crypto_generichash_blake2b(playload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
+        crypto_generichash_blake2b(payload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
     };break;
+
     }
 
     return true;
@@ -74,8 +97,8 @@ bool createaddress(std::uint8_t const *const input_key, InputKey const input_key
     char hrp[11];
     std::uint8_t header;
     std::uint8_t buff_xvk[XVK_LENGTH];
-    std::uint8_t playload_left[28];
-    std::uint8_t playload_right[28];
+    std::uint8_t payload_left[28];
+    std::uint8_t payload_right[28];
 
     ///Network::Mainnet=1, Network::Testnet=0
     header=static_cast<std::uint8_t>(network_id);
@@ -84,7 +107,7 @@ bool createaddress(std::uint8_t const *const input_key, InputKey const input_key
     if(address_type != Address::Stake){
         std::strcpy(hrp, "addr");
     }else{
-        std::strcpy(hrp,"stake");
+        std::strcpy(hrp, "stake");
     }
     if(network_id == Network::Testnet){
         std::strcat(hrp, "_test");
@@ -93,20 +116,20 @@ bool createaddress(std::uint8_t const *const input_key, InputKey const input_key
     ///Set Address
     if(!set_address(&address_type, input_key, &input_key_type, &wallet_type,
                     &account_path, &address_index_path, &header,
-                    playload_left, playload_right, buff_xvk)){
+                    payload_left, payload_right, buff_xvk)){
         return false;
     }
 
     buff_xvk[0] = header;
     if(address_type == Address::Base || address_type == Address::Base_Change ){
         for(std::uint8_t i = 0; i < 28; i++){
-            buff_xvk[1+i] = playload_left[i];
-            buff_xvk[29+i] = playload_right[i];
+            buff_xvk[1+i] = payload_left[i];
+            buff_xvk[29+i] = payload_right[i];
         }
         bech32_encode(hrp, buff_xvk, 57, address_out);
     }else{
         for(std::uint8_t i = 0; i < 28; i++){
-            buff_xvk[1 + i] = playload_left[i];
+            buff_xvk[1 + i] = payload_left[i];
         }
         bech32_encode(hrp, buff_xvk, 29, address_out);
     }
@@ -121,8 +144,8 @@ bool raw_createaddress_keyhash(std::uint8_t const *const input_key, InputKey con
 
     std::uint8_t header;
     std::uint8_t buff_xvk[XVK_LENGTH];
-    std::uint8_t playload_left[28];
-    std::uint8_t playload_right[28];
+    std::uint8_t payload_left[28];
+    std::uint8_t payload_right[28];
 
     ///Network::Mainnet=1, Network::Testnet=0
     header=static_cast<std::uint8_t>(network_id);
@@ -131,22 +154,22 @@ bool raw_createaddress_keyhash(std::uint8_t const *const input_key, InputKey con
     ///Set Address
     if(!set_address(&address_type, input_key, &input_key_type, &wallet_type,
                     &account_path, &address_index_path, &header,
-                    playload_left, playload_right, buff_xvk)){
+                    payload_left, payload_right, buff_xvk)){
         return false;
     }
 
     output_keyhash[0] = header;
     if(address_type == Address::Base || address_type == Address::Base_Change ){
         for(std::uint8_t i = 0; i < 28; i++){
-            output_keyhash[1+i] = playload_left[i];
-            output_keyhash[29+i] = playload_right[i];
+            output_keyhash[1+i] = payload_left[i];
+            output_keyhash[29+i] = payload_right[i];
             if(output_keyhash_len != nullptr){
                 *output_keyhash_len = 57;
             }
         }
     }else{
         for(std::uint8_t i = 0; i < 28; i++){
-            output_keyhash[1 + i] = playload_left[i];
+            output_keyhash[1 + i] = payload_left[i];
         }
         if(output_keyhash_len != nullptr){
             *output_keyhash_len = 29;
@@ -156,3 +179,4 @@ bool raw_createaddress_keyhash(std::uint8_t const *const input_key, InputKey con
 
     return true;
 }
+
--- Addresses/addresses.hpp
+++ Addresses/addresses.hpp
@@ -1,3 +1,48 @@
+/**
+MIT License
+
+Copyright (c) 2022 Eztero
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Documentation:
+https://cips.cardano.org/cips/cip5/
+https://cips.cardano.org/cips/cip19/
+https://hydra.iohk.io/build/16861845/download/1/ledger-spec.pdf (p. 111)
+**/
+
+/**
+
+-------------------------------------  Library functions  ---------------------------------------------------
+
+                                                                                                              Number(uint32)   Number(uint32)      String (addr1.., stake1..)
+                                                                                                                   :                :                :
+            createaddress(input_key, InputKey::ExtendedMasterKey, Network::Mainnet, Wallet::HD, Address::Base, account_path, address_index_path, address_out);
+raw_createaddress_keyhash(input_key, InputKey::ExtendedMasterKey, Network::Mainnet, Wallet::HD, Address::Base, account_path, address_index_path, output_keyhash, output_keyhash_len)
+                             :           :                            :               :             :                                                 :                :
+                          Master Key    0=ExtendedMasterKey       0=Testnet         0=HD         0=Base                                            uint8_t[output_keyhash_len]
+                          Account Key   1=AccountKey_xvk          1=Mainnet         1=MultiSign  1=Base_Change
+                                        2=AccountKey_xsk                                         2=Enterprise
+                                                                                                 3=Enterprise_Change
+                                                                                                 4=Stake
+
+**/
 
 #ifndef ADDRESSES_HPP
 #define ADDRESSES_HPP
