ca35a1b6a99f9dcd7637602bf1a964a7982dac18
Eztero <37254547+Eztero@users.noreply.github.com>
2023-11-22 20:42:43 -0300

    getRawAddressKeyHash function is added

--- include/cardanoplusplus/addresses/addresses.hpp
+++ include/cardanoplusplus/addresses/addresses.hpp
@@ -31,15 +31,15 @@ https://hydra.iohk.io/build/16861845/download/1/ledger-spec.pdf (p. 111)
 
 -------------------------------------  Library functions  ---------------------------------------------------
 
-                                                                                                         Number(uint32)   Number(uint32)    String (addr1.., stake1..)
-                                                                                                                :                :               :
-    getBech32Address(input_key, InputKey::ExtendedMasterKey, Network::Mainnet, Wallet::HD, Address::Base, account_path, address_index_path, address_out);
-       getRawAddress(input_key, InputKey::ExtendedMasterKey, Network::Mainnet, Wallet::HD, Address::Base, account_path, address_index_path, output_keyhash, output_keyhash_len)
-                             :           :                            :               :             :                                               :              :
-                          Master Key    0=ExtendedMasterKey       0=Testnet         0=HD         0=Base                                         uint8_t[output_keyhash_len]
-                          Account Key   1=AccountKey_xvk          1=Mainnet         1=MultiSign  1=Base_Change
-                                        2=AccountKey_xsk                                         2=Enterprise
-                                                                                                 3=Enterprise_Change
+                                                                                                                Number(uint32)  Number(uint32)     String (addr1.., stake1..)
+                                                                                                                      :                :                :
+    getBech32Address(input_key, InputKey::ExtendedMasterKey, Network::Mainnet, Wallet::HD, Address::Base_Extern, account_path, address_index_path, address_out);
+       getRawAddress(input_key, InputKey::ExtendedMasterKey, Network::Mainnet, Wallet::HD, Address::Base_Extern, account_path, address_index_path, output_keyhash, output_keyhash_len)
+                             :           :                            :               :             :                                                     :              :
+                          Master Key    0=ExtendedMasterKey       0=Testnet         0=HD         0=Base_Extern                                      uint8_t[output_keyhash_len]
+                          Account Key   1=AccountKey_xvk          1=Mainnet         1=MultiSign  1=Base_Intern
+                                        2=AccountKey_xsk                                         2=Enterprise_Extern
+                                                                                                 3=Enterprise_Intern
                                                                                                  4=Stake
 
 **/
@@ -56,22 +56,31 @@ namespace Cardano{
 // Both functions throw exceptions of type std::invalid_argument
 
 // get a addresses serialized in bech32, for example addr1v8....
-void getBech32Address(std::uint8_t const * const input_key, Cardano::InputKey const input_key_type, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
-                      std::uint32_t const account_path, std::uint32_t const address_index_path, std::string & address_out);
+void getBech32Address( Cardano::InputKey const input_key_type, std::uint8_t const * const input_key, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
+                       std::uint32_t const account_path, std::uint32_t const address_index_path, std::string & address_output );
 
 // get unserialized addresses
-void getRawAddress(std::uint8_t const * const input_key, Cardano::InputKey const input_key_type, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
-                               std::uint32_t const account_path, std::uint32_t const address_index_path,
-                               std::uint8_t * const output_raw, std::uint8_t * const output_raw_len);
+void getRawAddress( Cardano::InputKey const input_key_type, std::uint8_t const * const input_key, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
+                    std::uint32_t const account_path, std::uint32_t const address_index_path,
+                    std::uint8_t * const output_raw, std::uint8_t * const output_raw_len );
 
-void getBech32ScriptHash(std::uint8_t const * const input_script, std::size_t input_script_len, Cardano::ScriptType const script_type, std::string & address_out );
+// get a stake addr_keyhash
+void getRawAddressKeyHash( Cardano::InputKey const input_key_type, std::uint8_t const * const input_key, Cardano::Network const network_id,
+                           std::uint32_t const account_path,std::uint8_t * const addresskeyhash_output, std::uint8_t * const addresskeyhash_len );
 
+void getBech32ScriptHash( Cardano::ScriptType const script_type, std::string const &script, std::string & address_output );
 
-void getRawScriptHash(std::uint8_t const * const input_script, std::size_t input_script_len, Cardano::ScriptType const script_type,
-                      std::uint8_t * const output_28bytesraw, std::uint8_t * const output_28bytesraw_len );
+void getRawScriptHash( Cardano::ScriptType const script_type, std::string const &script,
+                       std::uint8_t * const scripthash_output, std::uint8_t * const scripthash_len );
 
-void getBech32AddressfromScript(std::string const &script, Cardano::ScriptType const script_type,  Cardano::Network const network_id,
-                                Cardano::ScriptAddress const address_type, std::string & address_out );
+// Create an address (Bech32) from a script
+void getBech32AddressfromScript( Cardano::ScriptType const script_type, std::string const &script, Cardano::Network const network_id,
+                                 Cardano::ScriptAddress const address_output_type, std::string & address_output );
+
+// Create a Base address (Bech32) from the union of an Enterprise/Base and Stake address
+void getBech32AddressfromAddresses( std::string const &payment_address, std::string const &stake_address,
+                                    std::string & address_output );
+
 
 }
 
--- src/addresses/addresses.cpp
+++ src/addresses/addresses.cpp
@@ -96,9 +96,7 @@ static void set_script(std::uint8_t const * const input_script, std::size_t cons
     int script_a_len = *input_script_len - pos_arrayofscript + 1;
     std::uint8_t script_a[script_a_len]{};
 
-    if(script_type != ScriptType::None){
-     script_a[0] = static_cast<std::uint8_t>(script_type);
-    }else{throw std::invalid_argument("Set Script error, ScriptType not supported ");}
+    script_a[0] = static_cast<std::uint8_t>(script_type);
 
     std::memcpy ( &script_a[1], &input_script[pos_arrayofscript], (*input_script_len - pos_arrayofscript));
     crypto_generichash_blake2b(output_scripthash, BLAKE224_LENGTH, script_a, script_a_len, nullptr, 0);
@@ -113,18 +111,18 @@ static void set_address(Address const * const address_type, std::uint8_t const *
     ///Set Address
     switch(*address_type){             ///bech32(hrp, (header || payload_left || payload_right) )
 
-    case Address::Base:{              /// Role = External Address
+    case Address::Base_Extern:{              /// Role = External Address
 
-        if(!getRawKey(input_key, input_key_type, wallet_type, OutputKey::Public,
-                      *account_path, Role::Extern, *address_index_path, buff_xvk)){
+        if(!getRawKey( input_key_type, input_key, wallet_type, OutputKey::Public,
+                       *account_path, Role::Extern, *address_index_path, buff_xvk ) ){
             throw std::invalid_argument("setting address error,  could not get a valid public key");
             //return false;
         }
 
         crypto_generichash_blake2b(payload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
-        if(!getRawKey(input_key, input_key_type, wallet_type, OutputKey::Public,
-                      *account_path, Role::Staking, STAKE_INDEX, buff_xvk)){
+        if(!getRawKey( input_key_type, input_key, wallet_type, OutputKey::Public,
+                       *account_path, Role::Staking, STAKE_INDEX, buff_xvk ) ){
             throw std::invalid_argument("setting address error,  could not get a valid public stake key");
             //return false;
         }
@@ -133,18 +131,18 @@ static void set_address(Address const * const address_type, std::uint8_t const *
 
     };break;
 
-    case Address::Base_Change:{       /// Role = Internal Address
+    case Address::Base_Intern:{       /// Role = Internal Address
 
-        if(!getRawKey(input_key, input_key_type, wallet_type, OutputKey::Public,
-                      *account_path, Role::Intern, *address_index_path, buff_xvk)){
+        if(!getRawKey( input_key_type, input_key, wallet_type, OutputKey::Public,
+                       *account_path, Role::Intern, *address_index_path, buff_xvk ) ){
             throw std::invalid_argument("setting address error, could not get a valid public key");
             //return false;
         }
 
         crypto_generichash_blake2b(payload_left, BLAKE224_LENGTH, buff_xvk, 32, nullptr, 0);
 
-        if(!getRawKey(input_key, input_key_type, wallet_type, OutputKey::Public,
-                      *account_path, Role::Staking, STAKE_INDEX, buff_xvk)){
+        if(!getRawKey( input_key_type, input_key, wallet_type, OutputKey::Public,
+                       *account_path, Role::Staking, STAKE_INDEX, buff_xvk ) ){
             throw std::invalid_argument("setting address error, could not get a valid public stake key");
             //return false;
         }
@@ -154,12 +152,12 @@ static void set_address(Address const * const address_type, std::uint8_t const *
 
     };break;
 
-    case Address::Enterprise:{        /// Role = External Address
+    case Address::Enterprise_Extern:{        /// Role = External Address
 
         *header |= 0x60; //b0110_0000
 
-        if(!getRawKey(input_key, input_key_type, wallet_type, OutputKey::Public,
-                      *account_path, Role::Extern, *address_index_path, buff_xvk)){
+        if(!getRawKey( input_key_type, input_key, wallet_type, OutputKey::Public,
+                       *account_path, Role::Extern, *address_index_path, buff_xvk ) ){
             throw std::invalid_argument("setting address error, could not get a valid public key");
             //return false;
         }
@@ -168,12 +166,12 @@ static void set_address(Address const * const address_type, std::uint8_t const *
 
     };break;
 
-    case Address::Enterprise_Change:{ /// Role = Internal Address
+    case Address::Enterprise_Intern:{ /// Role = Internal Address
 
         *header |= 0x60; //b0110_0000
 
-        if(!getRawKey(input_key, input_key_type, wallet_type, OutputKey::Public,
-                      *account_path, Role::Intern, *address_index_path, buff_xvk)){
+        if(!getRawKey( input_key_type, input_key, wallet_type, OutputKey::Public,
+                       *account_path, Role::Intern, *address_index_path, buff_xvk ) ){
             throw std::invalid_argument("setting address error, could not get a valid public key");
             //return false;
         }
@@ -186,8 +184,8 @@ static void set_address(Address const * const address_type, std::uint8_t const *
 
         *header |= 0xE0; //b1110_0000
 
-        if(!getRawKey(input_key, input_key_type, wallet_type, OutputKey::Public,
-                      *account_path, Role::Staking, *address_index_path, buff_xvk)){
+        if(!getRawKey( input_key_type, input_key, wallet_type, OutputKey::Public,
+                       *account_path, Role::Staking, *address_index_path, buff_xvk ) ){
             throw std::invalid_argument("setting address error, could not get a valid public stake key");
             //return false;
         }
@@ -201,9 +199,9 @@ static void set_address(Address const * const address_type, std::uint8_t const *
     //return true;
 }
 
-void getBech32Address(std::uint8_t const * const input_key, Cardano::InputKey const input_key_type, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
-                      std::uint32_t const account_path, std::uint32_t const address_index_path,
-                      std::string & address_out){
+void getBech32Address( Cardano::InputKey const input_key_type, std::uint8_t const * const input_key, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
+                       std::uint32_t const account_path, std::uint32_t const address_index_path,
+                       std::string & address_output){
 
     char hrp[11]{};
     std::uint8_t header;
@@ -238,24 +236,24 @@ void getBech32Address(std::uint8_t const * const input_key, Cardano::InputKey co
 
 
     buff_xvk[0] = header;
-    if(address_type == Address::Base || address_type == Address::Base_Change ){
+    if(address_type == Address::Base_Extern || address_type == Address::Base_Intern){
         for(std::uint8_t i = 0; i < 28; i++){
             buff_xvk[1+i] = payload_left[i];
             buff_xvk[29+i] = payload_right[i];
         }
-        Hash::bech32_encode(hrp, buff_xvk, 57, address_out);
+        Hash::bech32_encode(hrp, buff_xvk, 57, address_output);
     }else{
         for(std::uint8_t i = 0; i < 28; i++){
             buff_xvk[1 + i] = payload_left[i];
         }
-        Hash::bech32_encode(hrp, buff_xvk, 29, address_out);
+        Hash::bech32_encode(hrp, buff_xvk, 29, address_output);
     }
 
 }
 
-void getRawAddress(std::uint8_t const * const input_key, Cardano::InputKey const input_key_type, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
-                   std::uint32_t const account_path, std::uint32_t const address_index_path,
-                   std::uint8_t * const output_raw,std::uint8_t * const output_raw_len){
+void getRawAddress( Cardano::InputKey const input_key_type, std::uint8_t const * const input_key, Cardano::Network const network_id, Cardano::Wallet const wallet_type, Cardano::Address const address_type,
+                    std::uint32_t const account_path, std::uint32_t const address_index_path,
+                    std::uint8_t * const output_raw,std::uint8_t * const output_raw_len){
 
     std::uint8_t header;
     std::uint8_t buff_xvk[XVK_LENGTH]{};
@@ -279,7 +277,7 @@ void getRawAddress(std::uint8_t const * const input_key, Cardano::InputKey const
 
 
     output_raw[0] = header;
-    if(address_type == Address::Base || address_type == Address::Base_Change ){
+    if(address_type == Address::Base_Extern || address_type == Address::Base_Intern ){
         for(std::uint8_t i = 0; i < 28; i++){
             output_raw[1+i] = payload_left[i];
             output_raw[29+i] = payload_right[i];
@@ -298,55 +296,165 @@ void getRawAddress(std::uint8_t const * const input_key, Cardano::InputKey const
 
 }
 
-void getBech32ScriptHash(std::uint8_t const * const input_script, std::size_t input_script_len, Cardano::ScriptType const script_type, std::string & address_out ){
+void getRawAddressKeyHash( Cardano::InputKey const input_key_type, std::uint8_t const * const input_key,
+                           std::uint32_t const account_path,std::uint8_t * const addresskeyhash_output, std::uint8_t * const addresskeyhash_len ){
 
-    std::uint8_t blake_28bytes[BLAKE224_LENGTH]{};
-    set_script(input_script, &input_script_len, script_type,blake_28bytes);
+    std::uint8_t header=0;
+    std::uint8_t buff_xvk[XVK_LENGTH]{};
+    std::uint8_t payload_left[28]{};
+    std::uint8_t payload_right[28]{};
 
-    char hrp[11]{};
-    std::strcpy(hrp, "script");
-    Cardano::Hash::bech32_encode(hrp, blake_28bytes, BLAKE224_LENGTH, address_out);
+    std::uint32_t const address_index_path = 0;
+    Address const address_type = Address::Stake;
+    Wallet const wallet_type = Wallet::HD;
+
+    set_address(&address_type, input_key, input_key_type, wallet_type,
+                &account_path, &address_index_path, &header,
+                payload_left, payload_right, buff_xvk);
+
+
+
+    addresskeyhash_output[0] = header;
+
+    for(std::uint8_t i = 0; i < 28; i++){
+        addresskeyhash_output[i] = payload_left[i];
+    }
+    if(addresskeyhash_len != nullptr){
+        *addresskeyhash_len = 28;
+    }
+}
+
+void getBech32ScriptHash( Cardano::ScriptType const script_type, std::string const &script, std::string & address_output ){
+
+    std::size_t script_uint8_t_len = 0;
+    std::uint8_t const * const script_uint8_t = (script_type != ScriptType::None)?Utils::hexchararray2uint8array(script, &script_uint8_t_len):nullptr;
+
+    if(script_uint8_t != nullptr){
+        std::uint8_t blake_28bytes[BLAKE224_LENGTH]{};
+        set_script(script_uint8_t, &script_uint8_t_len, script_type,blake_28bytes);
+        delete[] script_uint8_t;
+        char hrp[11]{};
+        std::strcpy(hrp, "script");
+        if(!Hash::bech32_encode(hrp, blake_28bytes, BLAKE224_LENGTH, address_output)){
+            throw std::invalid_argument("getBech32ScriptHash error , could not encode in bech32 ");
+        }
+
+    }else{
+        throw std::invalid_argument("getBech32ScriptHash error , ScriptType not supported ");
+    }
 
 }
 
 // hash 28 bytes
-void getRawScriptHash(std::uint8_t const * const input_script, std::size_t input_script_len, Cardano::ScriptType const script_type,
-                      std::uint8_t * const output_28bytesraw, std::uint8_t * const output_28bytesraw_len ){
+void getRawScriptHash( Cardano::ScriptType const script_type, std::string const &script,
+                       std::uint8_t * const scripthash_output, std::uint8_t * const scripthash_len ){
 
-    set_script(input_script, &input_script_len, script_type,output_28bytesraw);
-    if(output_28bytesraw_len != nullptr){
-        *output_28bytesraw_len = BLAKE224_LENGTH;
+    std::size_t script_uint8_t_len = 0;
+    std::uint8_t const * const script_uint8_t = (script_type != ScriptType::None)?Utils::hexchararray2uint8array(script, &script_uint8_t_len):nullptr;
+
+    if(script_uint8_t != nullptr){
+        set_script(script_uint8_t, &script_uint8_t_len, script_type,scripthash_output);
+        if(scripthash_len != nullptr){
+            *scripthash_len = BLAKE224_LENGTH;
+        }
+        delete[] script_uint8_t;
+    }else{
+        throw std::invalid_argument("getRawScriptHash error , ScriptType not supported ");
     }
 
 }
 
-void getBech32AddressfromScript(std::string const &script, Cardano::ScriptType const script_type,  Cardano::Network const network_id,
-                                Cardano::ScriptAddress const address_type, std::string & address_out ){
+void getBech32AddressfromScript( Cardano::ScriptType const script_type, std::string const &script,  Cardano::Network const network_id,
+                                 Cardano::ScriptAddress const address_output_type, std::string & address_output ){
     char hrp[11]{};
-    std::uint8_t output_29bytesraw[29]{};
+    std::uint8_t payload[29]{};
     std::uint8_t output_28bytesraw[BLAKE224_LENGTH]{};
     std::size_t script_uint8_t_len = 0;
-    std::uint8_t const * const script_uint8_t = Utils::hexchararray2uint8array(script, &script_uint8_t_len);
-    try{
+    std::uint8_t const * const script_uint8_t = (script_type != ScriptType::None)?Utils::hexchararray2uint8array(script, &script_uint8_t_len):nullptr;
+
+    if(script_uint8_t != nullptr){
         set_script(script_uint8_t, &script_uint8_t_len, script_type, output_28bytesraw);
-    }catch(const std::invalid_argument& erl){
         delete[] script_uint8_t;
-        throw;
+    }else{
+        throw std::invalid_argument("getBech32AddressfromScript error , ScriptType not supported ");
     }
-    delete[] script_uint8_t;
-    output_29bytesraw[0] = 0x70; // enterprise address
-    output_29bytesraw[0] |= static_cast<std::uint8_t>(network_id);
 
-    std::memcpy ( &output_29bytesraw[1], output_28bytesraw, BLAKE224_LENGTH );
-
-    switch(address_type){
-    case ScriptAddress::Payment:{ std::strcpy(hrp, "addr"); };break;
-    case ScriptAddress::Stake:{ std::strcpy(hrp, "stake"); };break;
+    switch(address_output_type){
+    case ScriptAddress::Payment:{
+        std::strcpy(hrp, "addr");
+        payload[0] = 0x70 | static_cast<std::uint8_t>(network_id);; // enterprise address
+    };break;
+    case ScriptAddress::Stake:{
+        std::strcpy(hrp, "stake");
+        payload[0] = 0xf0 | static_cast<std::uint8_t>(network_id);; // stake address
+    };break;
     }
+
     if(network_id == Network::Testnet){
         std::strcat(hrp, "_test");
     }
-    Hash::bech32_encode(hrp, output_29bytesraw, 29, address_out);
+
+    std::memcpy ( &payload[1], output_28bytesraw, 28 );
+    Hash::bech32_encode(hrp, payload, 29, address_output);
+}
+
+void getBech32AddressfromAddresses( std::string const &payment_address, std::string const &stake_address,
+                                    std::string & address_output ){
+
+    std::uint8_t payload_payment[BECH32_MAX_LENGTH]{};
+    std::uint8_t payload_stake[BECH32_MAX_LENGTH]{};
+    std::uint8_t payload[57]{};
+    char hrp[11]{};
+
+    std::uint16_t payload_payment_len = 0;
+    std::uint16_t payload_stake_len = 0;
+
+    if(payment_address.find("addr")){
+        throw std::invalid_argument("getBech32AddressfromAddresses error , payment address does not contain a valid address");
+    }
+    if(stake_address.find("stake")){
+        throw std::invalid_argument("getBech32AddressfromAddresses error , stake address does not contain a valid address");
+    }
+
+    if(!Hash::bech32_decode(payment_address.c_str(),payload_payment,&payload_payment_len)){
+        throw std::invalid_argument("getBech32AddressfromAddresses error , could not decode payment address");
+    }
+
+    if(!Hash::bech32_decode(stake_address.c_str(),payload_stake,&payload_stake_len)){
+        throw std::invalid_argument("getBech32AddressfromAddresses error , could not decode stake address");
+    }
+
+    //verifica las cabezeras de las direcciones
+    if(payload_payment[0] >> 7){
+        throw std::invalid_argument("getBech32AddressfromAddresses error , payment address does not contain a valid payment header");
+    }
+
+    if(!(payload_stake[0] >> 7)){
+        throw std::invalid_argument("getBech32AddressfromAddresses error , stake address does not contain a valid payment header");
+    }
+
+    // se usa la direccion de pago para obtener la red en la que se trabaja mainnet/testnet
+    payload[0] = payload_payment[0] & 0x0f;
+    switch(payload[0]){
+    case 0x01:{
+        std::strcpy(hrp, "addr");
+    };break;
+    case 0x00:{
+        std::strcpy(hrp, "addr_test");
+    };break;
+    }
+
+    // Se crea el nuevo header a partir de las dos direcciones pago y stake
+    payload[0] |= ( payload_payment[0] & 0x10 );
+    payload[0] |= ( (payload_stake[0] & 0x10) << 1 );
+
+    for(std::uint8_t i = 0; i < 28; i++){
+        payload[1+i] = payload_payment[i+1];
+        payload[29+i] = payload_stake[i+1];
+    }
+
+    Hash::bech32_encode(hrp, payload, 57, address_output);
+
 }
 
 }
